323214dffec22da19a463ef44497078053aa0f81
Smailyn Martinez Rodriguez <smartinez@cegsoft.com>
2019-05-07 16:45:48 -0400

    fix local storage

--- PracticeApp/scripts/app.js
+++ PracticeApp/scripts/app.js
@@ -2,8 +2,9 @@
 let submitBtn = document.getElementById("add-task");
 let taskInput = document.getElementById("task-text");
 
-let localStorage = new localStorage();
+let localStorage = new LocalStorage();
 
 submitBtn.addEventListener('click', (event) => {
-    //localStorage.setItem()
+    localStorage.setItem()
+
 });
